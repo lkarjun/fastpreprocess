0d3dd85fe984ab96e88e797ed78cec3e2fd67a10
lkarjun <lk.arjun@hotmail.com>
2021-05-14 19:41:35 +0530

    Fixed ajax

---
+++ static/js/main.js
@@ -0,0 +1,34 @@
+upload = $(document).ready(function (e) {
+    $('#UploadForm').on('submit',(function(e) {
+        e.preventDefault();
+        var formData = new FormData();
+        formData.append('file', $('#fileData')[0].files[0])
+        var imgname  =  $('input[type=file]').val();
+        var size  =  $('#fileData')[0].files[0].size;
+        var type = $('#fileData')[0].files[0].type;
+        console.log(size);
+        console.log(imgname);
+        console.log(type);
+        $.ajax({
+            type:'POST',
+            url: '/edafileupload',
+            data:formData,
+            enctype: 'multipart/form-data',
+            cache:false,
+            contentType: false,
+            processData: false,
+            success:function(data){
+                console.log("success");
+                console.log(data);
+                $("#upload-sec").css('display', 'none');
+                $("#show-details").css('display', 'block')
+
+            },
+            error: function(data){
+                console.log("error");
+                console.log(data);
+            }
+        });
+    }));
+
+});
