6631b3150102923c71a2ca5b026426f6d11bcb8a
lkarjun <lk.arjun@hotmail.com>
2021-05-15 11:03:36 +0530

    quick stat created

--- fasteda.py
+++ fasteda.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from essential import SampleData, FileDetail
+from essential import *
+import numpy as np
+
 
 def file_columns(filedetail: FileDetail) -> SampleData:
     obj = filedetail.obj
@@ -7,6 +9,17 @@ def file_columns(filedetail: FileDetail) -> SampleData:
     return sample_data
 
 
+def quick_stat(filedetail: FileDetail) -> QuickStat:
+    stat = np.round(filedetail.obj.describe().values.tolist(), 3).tolist()
+    zipping = [(variable, data) for variable, data in zip(QuickStat().variables, stat)]
+    return QuickStat(stat, zipping)
+
+
 if __name__ == "__main__":
-    path = 'static/dataset/bank_data_processed.csv'
-    print(list(file_columns(path)[1]))
+    fd = FileDetail(
+                    'bank_data_processed.csv',
+                    'csv', 
+                    '500 bytes', 'static/dataset/bank_data_processed.csv', 
+                     pd.read_csv('static/dataset/bank_data_processed.csv'))
+    
+    print(quick_stat(fd).zipped)
