1c56dab3b3808a80d28fe55bc01fe654666daba2
lkarjun <lk.arjun@hotmail.com>
2021-05-15 08:23:00 +0530

    Class SampleData created

--- essential.py
+++ essential.py
@@ -1,8 +1,16 @@
-from typing import NamedTuple
+from typing import List, NamedTuple
 
 
 class FileDetail(NamedTuple):
     filename: str
     filetype: str
     filesize: str
+    sysfilepath: str
+    obj: object
 
+class SampleData(NamedTuple):
+    samplesize: int
+    sampledata: List
+    columns: List[str]
+    number_of_columns: int
+    length: int
