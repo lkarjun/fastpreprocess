37c2894a94f18a85452704772c6a086df032a58d
lkarjun <lk.arjun@hotmail.com>
2021-05-15 11:12:52 +0530

    FastEda class Updated

--- fasteda.py
+++ fasteda.py
@@ -1,18 +1,23 @@
-import pandas as pd
 from essential import *
 import numpy as np
+import pandas as pd
 
 
-def file_columns(filedetail: FileDetail) -> SampleData:
-    obj = filedetail.obj
-    sample_data = SampleData(10, obj.sample(10).values.tolist(), obj.columns, len(obj.columns), len(obj))
-    return sample_data
+class FastEda:
+
+    def __init__(self, file: FileDetail) -> None:
+        self.file = file
 
 
-def quick_stat(filedetail: FileDetail) -> QuickStat:
-    stat = np.round(filedetail.obj.describe().values.tolist(), 3).tolist()
-    zipping = [(variable, data) for variable, data in zip(QuickStat().variables, stat)]
-    return QuickStat(stat, zipping)
+    def file_columns(self) -> SampleData:
+        obj = self.file.obj
+        sample_data = SampleData(10, obj.sample(10).values.tolist(), obj.columns, len(obj.columns), len(obj))
+        return sample_data
+    
+    def quick_stat(self) -> QuickStat:
+        stat = np.round(self.file.obj.describe().values.tolist(), 3).tolist()
+        zipping = [(variable, data) for variable, data in zip(QuickStat().variables, stat)]
+        return QuickStat(stat, zipping)
 
 
 if __name__ == "__main__":
@@ -22,4 +27,6 @@ if __name__ == "__main__":
                     '500 bytes', 'static/dataset/bank_data_processed.csv', 
                      pd.read_csv('static/dataset/bank_data_processed.csv'))
     
-    print(quick_stat(fd).zipped)
+    fasteda = FastEda(fd)
+
+    print(fasteda.file_columns())
