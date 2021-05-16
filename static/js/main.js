5765645394bf191b5de9e42ff45d18672a515f13
lkarjun <lk.arjun@hotmail.com>
2021-05-16 07:47:36 +0530

    Module removed

--- static/js/main.js
+++ static/js/main.js
@@ -1,89 +1,85 @@
-window.ApexCharts = require('apexcharts');
-console.log(window.ApexCharts); // should return modules  
+import "apexcharts";
 
-
-function corr(){
-    var options = {
-        series: [{
-        name: 'Metric1',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric2',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric3',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric4',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric5',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric6',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric7',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric8',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      },
-      {
-        name: 'Metric9',
-        data: generateData(18, {
-          min: 0,
-          max: 90
-        })
-      }
-      ],
-        chart: {
-        height: 350,
-        type: 'heatmap',
-      },
-      dataLabels: {
-        enabled: false
-      },
-      colors: ["#008FFB"],
-      title: {
-        text: 'HeatMap Chart (Single color)'
-      },
-      };
-  
-      var chart = new ApexCharts(document.querySelector("#chart"), options);
-      chart.render();
+var options = {
+  series: [{
+  name: 'Metric1',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric2',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric3',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric4',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric5',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric6',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric7',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric8',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
+},
+{
+  name: 'Metric9',
+  data: generateData(18, {
+    min: 0,
+    max: 90
+  })
 }
+],
+  chart: {
+  height: 350,
+  type: 'heatmap',
+},
+dataLabels: {
+  enabled: false
+},
+colors: ["#008FFB"],
+title: {
+  text: 'HeatMap Chart (Single color)'
+},
+};
+
+var chart = new ApexCharts(document.querySelector("#myChart"), options);
+chart.render();
 
 $(document).ready(function (e) {
     $('#UploadForm').on('submit',(function(e) {
