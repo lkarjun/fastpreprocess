quick stat numerical columns fixed

fasteda.py
@@ -38,28 +38,26 @@ class FastEda:
     
     def quick_stat(self) -> QuickStat:
         stat = np.round(self.file.obj.describe().values.tolist(), 3).tolist()
+        numerical_col = self.file.obj.select_dtypes(exclude = ['object']).columns.to_list()
         zipping = [(variable, data) for variable, data in zip(QuickStat().variables, stat)]
-        return QuickStat(stat, zipping)
+        return QuickStat(stat, numerical_col, zipping)
 
     def correlation(self) -> Correlation:
         v = self.obj.corr().index.to_list()
         corr = np.round(self.obj.corr().values, 2).tolist()
         return Correlation(variable = v,
-                            correlation = corr,
-                            zipped = zip(v, corr))
+                            correlation = corr)
 
 
 if __name__ == "__main__":
     fd = FileDetail(
                     'bank_data_processed.csv',
                     'csv', 
-                    '500 bytes', 'static/dataset/bank_data_processed.csv', 
-                     pd.read_csv('static/dataset/bank_data_processed.csv'))
+                    '500 bytes', 'static/dataset/cars.csv', 
+                     pd.read_csv('static/dataset/cars.csv'))
     
     fasteda = FastEda(fd)
-    print(fasteda.correlation().zipped)
-    for i, j in fasteda.correlation().zipped:
-        print(i, j)
+    print(fasteda.quick_stat().numerical_col)
     # process = IndividualVariable(fd)
     # process.start()
     # print(process.full_variables.length)