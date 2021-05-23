9e8eff8b0b2885778e8635a0c17a1d4d293d7f16
lkarjun <lk.arjun@hotmail.com>
2021-05-23 09:50:36 +0530

    Introduced drop variable getdummy

--- fastwork.py
+++ fastwork.py
@@ -14,14 +14,59 @@ app.mount("/static", StaticFiles(directory="static"), name='static')
 templates = Jinja2Templates(directory="template")
 
 
+
+@app.get('/')
+async def home(requset: Request):
+    testing()
+    return process_data(requset)
+
+
+@app.get('/drop')
+def drop_column(column):
+    filedetail.objcopy = filedetail.objcopy.drop(column, axis=1)
+    return "okay"
+
+@app.get('/view')
+def view_new_ds(request: Request):
+    return templates.TemplateResponse('ViewNewDataset.html', 
+                        context={'request': request, 'sample': fasteda.sample(new=True)})
+
+@app.get('/get_dummy')
+def get_dummy(column):
+    filedetail.objcopy = pd.get_dummies(filedetail.obj, columns=[column])
+    return "Okay"
+
+
+@app.get('/testing')
+async def home(requset: Request):
+    return templates.TemplateResponse('testing.html', context={'request': requset})
+
+
+def testing():
+    filename = 'static/dataset/sample.csv'
+    dm = ','
+    df = pd.read_csv(filename, delimiter=dm)
+
+    global filedetail
+    filedetail = FileDetail(filename = filename[15:],
+                        filetype = filename.split('.')[-1], 
+                            filesize=f"{os.stat(filename).st_size} bytes", 
+                            sysfilepath=filename, 
+                            obj=df,
+                            missing = df.isna().sum().values.sum(),
+                            objcopy = df.copy())
+    
+
+
 def process_data(request: Request):
     try:
+        global fasteda
         fasteda = FastEda(filedetail)
         process = fasteda.process
         return templates.TemplateResponse('FastEda.html', 
                 context={'request': request, 'title': 'Workspace', 
                         'fname': filedetail.filename,\
-                        'sample': fasteda.file_columns(),\
+                        'sample': fasteda.sample(),\
                         'quick': fasteda.quick_stat(),\
                         'corr': fasteda.correlation(),\
                         'process': process})
@@ -31,9 +76,8 @@ def process_data(request: Request):
         return templates.TemplateResponse('error_file.html', context={'request': request, 'error': str(e)})
 
 
-@app.get('/')
-async def home(requset: Request):
-    return process_data(requset)
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 
 def start(filename: str, dm=',', port = 8000):
@@ -87,8 +131,4 @@ def run_from_notebook():
     '''
     nest_asyncio.apply()
 
-
-@app.get('/testing')
-async def home(requset: Request):
-    return templates.TemplateResponse('testing.html', context={'request': requset})
 
