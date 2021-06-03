fc28bad26dd85c98e77d0b74cdffbcc52fed46a9
lkarjun <lk.arjun@hotmail.com>
2021-06-03 12:08:39 +0530

    v0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="fastpreprocess",
-    version="0.1.7",
+    version="0.1.8",
     entry_points = {
         'console_scripts': [
             'fastpreprocess = fastpreprocess.fastwork:run_from_local',
